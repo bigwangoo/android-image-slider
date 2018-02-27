2ca50ac495286b272d6faaa6f8d29dcaad77a53a
wangyd <467863144@qq.com>
2018-02-27 09:21:43 +0800

    v1.0.3

--- slider/src/main/java/com/tianxiabuyi/slider/SliderTypes/TextSliderView.java
+++ slider/src/main/java/com/tianxiabuyi/slider/SliderTypes/TextSliderView.java
@@ -1,9 +1,11 @@
 package com.tianxiabuyi.slider.SliderTypes;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.tianxiabuyi.slider.R;
@@ -21,8 +23,13 @@ public class TextSliderView extends BaseSliderView {
     public View getView() {
         View v = LayoutInflater.from(getContext()).inflate(R.layout.slider_render_type_text, null);
         ImageView ivSlide = (ImageView) v.findViewById(R.id.daimajia_slider_image);
+        LinearLayout descriptionLayout = (LinearLayout) v.findViewById(R.id.description_layout);
         TextView tvDesc = (TextView) v.findViewById(R.id.description);
-        tvDesc.setText(getDescription());
+        if (TextUtils.isEmpty(getDescription())) {
+            descriptionLayout.setVisibility(View.GONE);
+        } else {
+            tvDesc.setText(getDescription());
+        }
         bindEventAndShow(v, ivSlide);
         return v;
     }
